ef4713af56c86c222dbd763daa930e6eaae9568b
Sarthak <16374350+sarthakgup@users.noreply.github.com>
2021-03-09 12:46:47 -0800

    Add files via upload

---
+++ ForLoopPractice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2ForLoop
+{
+    class ForLoopPractice
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for(i=2; i<101; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+        }
+    }
+}
