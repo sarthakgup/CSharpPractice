35f8e9607d2a4a65b76fda4421e5e046e6aa1b82
Sarthak <16374350+sarthakgup@users.noreply.github.com>
2021-03-09 12:45:52 -0800

    Properties of a circle

---
+++ Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Circle
+    {
+        private double radius;
+        private double diameter;
+        private double area;
+
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+                     
+        }
+        public double Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+        }
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+                diameter = value * 2; //d=2r
+                area = Math.Pow(radius, 2) * Math.PI; //Pi*r^2
+            }
+        }
+
+        public Circle()
+        {
+            this.Radius = 1;
+        }
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+            
+        }
+    }  
+}
